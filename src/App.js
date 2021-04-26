716f9d7571de1126389b083b2370a610943b63b2
MarGolNZ <54025171+MarGolNZ@users.noreply.github.com>
2021-04-26 19:48:12 +1000

    Add footer with GitHub link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World
+      <h2>Weather App</h2>
+
+      <footer>
+This project is created by Marina Golovina and is
+      
+      <a href="https://github.com/MarGolNZ/react-weather-application" target="_blank"> open-sourced on GitHub</a>
+    </footer>
     </div>
   );
 }
