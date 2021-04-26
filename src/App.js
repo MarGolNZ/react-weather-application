d2b454cd78ce7d64917f6442ac532a89e1cbed1f
MarGolNZ <54025171+MarGolNZ@users.noreply.github.com>
2021-04-26 20:18:09 +1000

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Weather App</h2>
+export default function App() {
 
-      <footer>
-This project is created by Marina Golovina and is
-      
-      <a href="https://github.com/MarGolNZ/react-weather-application" target="_blank"> open-sourced on GitHub</a>
-    </footer>
+  return (
+
+    <div className="App">
+
+        <div class="container">
+
+        <h2>Weather App</h2>
+
+        <footer>
+          This project is created by Marina Golovina and is
+        
+        <a href="https://github.com/MarGolNZ/react-weather-application" target="_blank"> open-sourced on GitHub</a>
+      </footer>
+      </div>
+
     </div>
   );
+
 }
 
-export default App;
+
