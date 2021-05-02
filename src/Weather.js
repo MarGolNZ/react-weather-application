ca8032756f9a7cd489d8f957aebcb9cf153c97e8
MarGolNZ <54025171+MarGolNZ@users.noreply.github.com>
2021-05-02 17:22:56 +1000

    Replace fake icon URL with real API URL

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import "./Weather.css";
-import FormattedDate from "./FormattedDate"
+
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios"
 
 export default function Weather(props) {
+    const [city, setCity] = useState(props.defaultCity)
     const [weatherData, setWeatherData] = useState({ ready: false })
 
     function handleResponse(response) {
@@ -16,21 +18,39 @@ export default function Weather(props) {
             city: response.data.name,
             description: response.data.weather[0].description,
             humidity: response.data.main.humidity,
-            iconUrl: "#"
-
+            iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@4x.png`
         })
-        
-        
+    }
+
+    function search() {
+       const apiKey = "8ea9a418f9dd13e967a728a357801a35";
+       let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+       axios.get(apiUrl).then(handleResponse)  
+    }
+
+    function handleSubmit(event) {
+       event.preventDefault()
+       search()
+    }
+
+    function handleCityChange(event) {
+        setCity(event.target.value);
     }
 
     if (weatherData.ready) {
     return (
         <div className="Weather">
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="row">
                     <div className="col-9">
-                        <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on"/>
+                        <input
+                        type="search"
+                        placeholder="Enter a city..."
+                        className="form-control"
+                        autoFocus="on"
+                        onChange={handleCityChange}
+                        />
                     </div>
                     <div className="col-3">
                         <button type="button" class="btn btn-primary w-100">Search</button>
@@ -40,53 +60,12 @@ export default function Weather(props) {
 
             </form>
 
-            <h1>{weatherData.city}</h1>
-
-            <ul>
-                <li>
-                    <FormattedDate date={weatherData.date} />
-                </li>
-                <li className="text-capitalize">{weatherData.description}</li>
-            </ul>  
-
-            <div className="row mt-3">
-
-               <div className="col-6" >
-                   <div className="clearfix">
-                       <img
-                       src={weatherData.iconUrl}
-                       alt={weatherData.description}
-                       className="float-left"
-                       />
-                       <div className="float-left">
-                       <span className="temperature">{Math.round(weatherData.temperature)}</span>
-                       <span className="unit">°C</span>
-                       </div>
-                    </div>
-               </div>
-
-               <div className="col-6">
-                   <ul>
-                       
-                       <li>
-                          Humidity: {weatherData.humidity}% 
-                       </li>
-                       <li>
-                          Wind: {Math.round(weatherData.wind)}km/h 
-                       </li>
-                   </ul>
-               </div>
-
-            </div>
+            <WeatherInfo data ={weatherData} />
         </div>
-    )
+    );
     } else {
-    const apiKey = "8ea9a418f9dd13e967a728a357801a35";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`
-    axios.get(apiUrl).then(handleResponse)   
-    return"Loading"
+        search()
+        return"Loading"
     }
-
-    
-    
+   
 }
