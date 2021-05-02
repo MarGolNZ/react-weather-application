d13bc54243183557bcb87fcbe4e7d65440e21bb1
MarGolNZ <54025171+MarGolNZ@users.noreply.github.com>
2021-05-02 18:09:26 +1000

    Customise weather icon with animated icons

--- src/Weather.js
+++ src/Weather.js
@@ -18,7 +18,7 @@ export default function Weather(props) {
             city: response.data.name,
             description: response.data.weather[0].description,
             humidity: response.data.main.humidity,
-            iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@4x.png`
+            icon: response.data.weather[0].icon
         })
     }
 
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react"
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon"
 
 export default function WeatherInfo(props) {
     return (
@@ -18,11 +19,12 @@ export default function WeatherInfo(props) {
 
                <div className="col-6" >
                    <div className="clearfix">
-                       <img
-                       src={props.data.iconUrl}
-                       alt={props.data.description}
-                       className="float-left"
-                       />
+                       <div class="float-left">
+                        <WeatherIcon
+                           code={props.data.icon}
+                        />
+                       </div>
+                       
                        <div className="float-left">
                        <span className="temperature">{Math.round(props.data.temperature)}</span>
                        <span className="unit">°C</span>
