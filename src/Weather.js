86d355f96de19b667b3afd68a130c1b7f84ac385
MarGolNZ <54025171+MarGolNZ@users.noreply.github.com>
2021-05-19 20:41:14 +1000

    Fix URL link

--- src/Weather.js
+++ src/Weather.js
@@ -25,7 +25,7 @@ export default function Weather(props) {
 
     function search() {
        const apiKey = "8ea9a418f9dd13e967a728a357801a35";
-       let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+       let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
        axios.get(apiUrl).then(handleResponse)  
     }
 
